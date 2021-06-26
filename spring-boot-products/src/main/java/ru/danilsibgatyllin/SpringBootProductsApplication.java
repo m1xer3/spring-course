d8be56ca801de05b497b9c82a51ea9ce7e8d0adc
m1xer3 <danilsibgatyllin@gmail>
2021-06-26 12:32:56 +0300

    Complite homework lesson 4

---
+++ spring-boot-products/src/main/java/ru/danilsibgatyllin/SpringBootProductsApplication.java
@@ -0,0 +1,13 @@
+package ru.danilsibgatyllin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProductsApplication.class, args);
+    }
+
+}
---
+++ spring-boot-products/src/test/java/ru/danilsibgatyllin/SpringBootProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.danilsibgatyllin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
